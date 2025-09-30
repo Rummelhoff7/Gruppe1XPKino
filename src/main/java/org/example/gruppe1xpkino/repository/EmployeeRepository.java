7a67afa1a4dd9b0c930f1a0aa3502e11fb9bf001
Joadoa1992 <jolu0001@stud.kea.dk>
2025-09-30 11:30:40 +0200

    EmployeeRepository

--- src/main/java/org/example/gruppe1xpkino/repository/EmployeeRepository.java
+++ src/main/java/org/example/gruppe1xpkino/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package org.example.gruppe1xpkino.repository;
 
-import org.aspectj.apache.bcel.Repository;
-import org.example.gruppe1xpkino.model.Employee;
 
-public class EmployeeRepository extends Repository<Employee, Integer> {
+import org.example.gruppe1xpkino.model.Employee;
+import org.springframework.data.repository.Repository;
+
+public interface EmployeeRepository extends Repository<Employee, Integer> {
+
 }
